f6baee2960fd701011a6cf741751b3e345c0ec1a
Kevin Maslowski <kevinmaslowski@gmail.com>
2022-07-06 16:28:54 +0200

    init new portfolio from scratch with nextjs

---
+++ Components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Footer: NextPage = () => {
+  return (
+    <footer>
+      <p>Hello from Footer.tsx</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ Components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Header: NextPage = () => {
+  return (
+    <header>
+      <p>Hello from Header.tsx</p>
+    </header>
+  );
+};
+
+export default Header;
