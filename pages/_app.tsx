cfb5545b14ba0ba7e8c9eb4ef9ca6367dedf3a14
Kevin Maslowski <kevinmaslowski@gmail.com>
2022-07-07 20:53:38 +0200

    setup header and footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,17 @@
 import "../styles/globals.css";
 import "../styles/reset.css";
 import type { AppProps } from "next/app";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header></Header>
+      <Component {...pageProps} />
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default MyApp;
